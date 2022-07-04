033648c2f4fb92f269124bf0e1d068b1bd50010b
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-07-04 12:52:23 +0545

    Added Bullet Death and Particle effect to enemy

--- first project/Assets/Scripts/Shooting/Bullet.cs
+++ first project/Assets/Scripts/Shooting/Bullet.cs
@@ -1,13 +1,29 @@
-using System;
 using UnityEngine;
 
 namespace Shooting
 {
     public class Bullet : MonoBehaviour
     {
+        private const string Enemy = "Enemy";
+
+        [SerializeField]
+        private ParticleSystem hitParticle;
+    
         private void OnEnable()
         {
             Destroy(gameObject, 3);
         }
+        
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (!col.CompareTag(Enemy)) return;
+            
+            var particle = Instantiate(hitParticle, transform.position, Quaternion.identity);
+            particle.Play();
+            Destroy(gameObject);
+            Destroy(col.gameObject);
+        }
+        
+        
     }
 }
