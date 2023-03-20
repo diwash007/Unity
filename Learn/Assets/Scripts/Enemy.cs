cd35ceddd3d4b93737a33cdc674ddc58acfdae9c
Diwash <diwashdahal75@gmail.com>
2023-03-20 19:38:35 +0545

    feat: player can kill enemies

--- Learn/Assets/Scripts/Enemy.cs
+++ Learn/Assets/Scripts/Enemy.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    [SerializeField] private float speed = 5;
+    [SerializeField] private float speed = 5f;
     [SerializeField]
     private Rigidbody2D rb;
+    [SerializeField]
+    private CapsuleCollider2D capsuleCollider;
 
     private void FixedUpdate()
     {
         rb.velocity = Vector2.left * speed * Time.deltaTime; ;
+    }
+    public void Die()
+    {
+        Destroy(gameObject,1);
+        speed = 0f;
+        capsuleCollider.size = new Vector2(0.001f, 0.001f);
+        transform.localScale = new Vector2(transform.localScale.x, 1f);
+        rb.gravityScale =100f;
     }
 }
---
+++ Learn/Assets/Scripts/EnemyKiller.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class EnemyKiller : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Enemy")) {
+            var enemy = other.gameObject.GetComponent<Enemy>();
+            enemy.Die();
+        }
+    }
+}
